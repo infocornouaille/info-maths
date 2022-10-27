022eaa27d9bdf1c1e161d81e76821d768d555563
infocornouaille <couronne.david@gmail.com>
2022-10-27 15:26:29 +0200

    Update katex.js

--- docs/javascripts/katex.js
+++ docs/javascripts/katex.js
@@ -1,43 +1,46 @@
-(function () {
-    'use strict';
+;(function () {
+  'use strict'
 
-    const macros = {
-        "\\RR": "\\mathbb{R}",
-        "\\vect": "{\\begin{pmatrix}#1\\\\#2\\end{pmatrix}}"
-    };
+  const macros = {
+    '\\RR': '\\mathbb{R}',
+    '\\vect': '{\\begin{pmatrix}#1\\\\#2\\end{pmatrix}}',
+  }
 
-    var katexMath = (function () {
-        var maths = document.querySelectorAll('.arithmatex'),
-            tex;
+  var katexMath = function () {
+    var maths = document.querySelectorAll('.arithmatex'),
+      tex
 
-        for (var i = 0; i < maths.length; i++) {
-            tex = maths[i].textContent || maths[i].innerText;
-            if (tex.startsWith('\\(') && tex.endsWith('\\)')) {
-                katex.render(tex.slice(2, -2), maths[i], { 'displayMode': false, macros, throwOnError: false });
-            } else if (tex.startsWith('\\[') && tex.endsWith('\\]')) {
-                katex.render(tex.slice(2, -2), maths[i], { 'displayMode': true, macros, throwOnError: false });
-            }
-        }
-    });
+    for (var i = 0; i < maths.length; i++) {
+      tex = maths[i].textContent || maths[i].innerText
+      if (tex.startsWith('\\(') && tex.endsWith('\\)')) {
+        katex.render(tex.slice(2, -2), maths[i], {
+          displayMode: false,
+          macros,
+          throwOnError: false,
+        })
+      } else if (tex.startsWith('\\[') && tex.endsWith('\\]')) {
+        katex.render(tex.slice(2, -2), maths[i], { displayMode: true, macros, throwOnError: false })
+      }
+    }
+  }
 
-    (function () {
-        var onReady = function onReady(fn) {
-            if (document.addEventListener) {
-                document.addEventListener("DOMContentLoaded", fn);
-            } else {
-                document.attachEvent("onreadystatechange", function () {
-                    if (document.readyState === "interactive") {
-                        fn();
-                    }
-                });
-            }
-        };
+  ;(function () {
+    var onReady = function onReady(fn) {
+      if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', fn)
+      } else {
+        document.attachEvent('onreadystatechange', function () {
+          if (document.readyState === 'interactive') {
+            fn()
+          }
+        })
+      }
+    }
 
-        onReady(function () {
-            if (typeof katex !== "undefined") {
-                katexMath();
-            }
-        });
-    })();
-
-}());
+    onReady(function () {
+      if (typeof katex !== 'undefined') {
+        katexMath()
+      }
+    })
+  })()
+})()
